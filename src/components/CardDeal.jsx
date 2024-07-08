0c797b942f0ed110d084e133bbbc9522119d25b3
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-08 14:20:19 +0200

    Update CardDeal.jsx

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import styles from "../style";
+import Button from "./Button";
+import card from "../assets/card.png";
 const CardDeal = () => {
-  return <div>CardDeal</div>;
+  return (
+    <section
+      id="card-deal"
+      className="flex flex-col justify-between items-center sm:flex-row "
+    >
+      <div className="text-white w-full sm:w-[50%]">
+        <h1 className="text-6xl">Find a better card deal in few easy steps.</h1>
+        <p className={`${styles.paragraph} mt-5`}>
+          Arcu tortor, purus in mattis at sed integer faucibus. Aliquet quis
+          aliquet eget mauris tortor.ç Aliquet ultrices ac, ametau.
+        </p>
+        <Button />
+      </div>
+      <div>
+        <img src={card} alt="card-image" className="w-[50vh] h-[45vh]" />
+      </div>
+    </section>
+  );
 };
 
 export default CardDeal;
