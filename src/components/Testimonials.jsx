ff450eb9468bc2d0d0eea5d1c81dd29524510759
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-08 14:49:11 +0200

    Update Testimonials.jsx

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,31 @@
-import React from "react";
-
+import { feedback } from "../data";
+import FeedbackCard from "./FeedbackCard";
+import styles from "../style.js";
 const Testimonials = () => {
-  return <div></div>;
+  return (
+    <section id="tesimonials" className="flex flex-col gap-10">
+      <div className="text-white flex justify-between items-center mt-10">
+        <h1 className="text-5xl font-semibold">
+          What people are <br /> saying about us
+        </h1>
+        <p className={`${styles.paragraph}`}>
+          Everything you need to accept card payments <br /> and grow your
+          business anywhere on the planet.
+        </p>
+      </div>
+      <div className="flex gap-5 justify-center mt-10">
+        {feedback.map((feed) => (
+          <FeedbackCard
+            key={feed.id}
+            img={feed.img}
+            content={feed.content}
+            name={feed.name}
+            title={feed.title}
+          />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
