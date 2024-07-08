2e3558808d9179344bb2c2ab1ca99a08fc64783c
Hasan Alić <87580658+H4S0@users.noreply.github.com>
2024-07-08 14:49:23 +0200

    Update FeedbackCard.jsx

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-
-const FeedbackCard = () => {
-  return <div></div>;
+import styles from "../style";
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex flex-col text-white hover:bg-slate-800 rounded-xl p-10 items-center justify-between gap-10">
+      <div>
+        <p className={`w-[30vh] ${styles.paragraph}`}>{content}</p>
+      </div>
+      <div className="flex flex-row items-center gap-5">
+        <img src={img} alt="image" width={80} height={80} />
+        <div className="flex flex-col">
+          <h1>{name}</h1>
+          <p className={`${styles.paragraph}`}>{title}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default FeedbackCard;
